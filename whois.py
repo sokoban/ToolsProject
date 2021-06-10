4ea3d4d357714309360e883d59d08ed35966375e
sokoban@naver.com <sokoban@naver.com>
2021-06-10 11:14:26 +0900

    whois

--- whois.py
+++ whois.py
@@ -2,30 +2,22 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-from netaddr import *
-from socket import *
-import subprocess
 
-def portscan(targetip, targetport):
-    MESSAGE = "ping"
-    sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
-    sock.settimeout(2)
-    try:
-        sock.sendto(MESSAGE.encode('utf_8'), (targetip, int(targetport)))
-        #result = sock.connect_ex( ( targetip , int(targetport) ) )
-        sock.recvfrom(1024)
-        print('[+] {} , {}/tcp open'.format(targetip, str(targetport)))
-    except Exception as e:
-        print(e)
-        print('[+] {} , {}/tcp close'.format(targetip, str(targetport)))
-
-def All_ipv4_address(subnet):
-    ipaddr = IPNetwork(subnet)
-    return ipaddr
+import whois
+from ipwhois import IPWhois
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("TEST")
+
+    obj = IPWhois('74.125.225.229')
+    results = obj.lookup_rdap(depth=1)
+    print(results)
+    #print(obj)
+
+#    domain = "www.naver.com"
+#    ret = whois.whois(domain)
+ #   print(ret)
 
 
 
